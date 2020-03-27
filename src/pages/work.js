65170572ce6314c6b08a0b793e100753f5c9a081
Michaël Bontyes <michaelbontyes@gmail.com>
2020-03-27 12:07:26 -0400

    layout fix

--- src/pages/work.js
+++ src/pages/work.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Link } from "gatsby"
 import { Helmet } from 'react-helmet'
 
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
