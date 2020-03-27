31b1e306aae0715ed0d9163b02438a0f9de297a0
Michaël Bontyes <michaelbontyes@gmail.com>
2020-03-27 11:05:32 -0400

    gatsby reset

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,23 +1,3 @@
-import './node_modules/uikit/dist/css/uikit.min.css'
-import './src/theme/gatsby-uikit/style.css'
-
-import './src/wp-blocks/style.min.css'
-import './src/wp-blocks/jetpack.css'
-import './src/wp-blocks/jetpack-carousel.min.css'
-
-import './node_modules/@fortawesome/fontawesome-free/css/all.min.css'
-import './node_modules/@fortawesome/fontawesome-free/css/fontawesome.min.css'
-import './node_modules/@fortawesome/fontawesome-free/css/brands.min.css'
-import './node_modules/@fortawesome/fontawesome-free/css/regular.min.css'
-
-import './node_modules/uikit/dist/js/uikit.min.js'
-import './node_modules/uikit/dist/js/uikit-icons.min.js'
-
-import './node_modules/@fortawesome/fontawesome-free/js/all.min.js'
-import './node_modules/@fortawesome/fontawesome-free/js/fontawesome.min.js'
-import './node_modules/@fortawesome/fontawesome-free/js/brands.min.js'
-import './node_modules/@fortawesome/fontawesome-free/js/regular.min.js'
-
 /**
  * Implement Gatsby's Browser APIs in this file.
  *
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,81 +1,21 @@
-import React, { Component } from "react"
-import { Link, graphql } from "gatsby"
+import React from "react"
+import { Link } from "gatsby"
 
+import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
 
-import Layout from '../components/Layout'
-import PostList from '../components/PostList'
-import NavBar from '../components/Navbar'
-
-import Hero from "../components/hero"
-
-class Homepage extends Component {
-  render() {
-    const data = this.props.data
-    const { edges: posts, totalCount } = data.allWordpressPost
-    const { edges: pages } = data.allWordpressPage
-    const { title: siteTitle } = data.site.siteMetadata
-    const title = `${totalCount} post${
-      totalCount === 1 ? '' : 's'
-    } `
-    return (
-    <>
-
-        <Layout pages={pages}>
-          <SEO title="Home" />
-          <div className="uk-section">
-              <div className="uk-container">
-                <div className="uk-flex uk-flex-center" data-uk-scrollspy="target: > div; cls: uk-animation-fade; delay: 100">
-                  <div className="uk-width-2-3@s">
-                    <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </h4>
-                  </div>
-                </div>
-              </div>
-          </div>
-          <div data-uk-scrollspy="target: > div; cls: uk-animation-fade; delay: 200" >
-          <Hero/>
-          </div>
-          <PostList posts={posts} title="" />
-
-        </Layout>
-
-
-
-        ))}
-      </>
-    )
-  }
-}
-
-export default Homepage
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    allWordpressPost(filter: {categories: {elemMatch: {slug: {eq: "portfolio"}}}}, sort: {fields: date, order: DESC})
-    {
-      totalCount
-      edges {
-        node {
-          ...PostListFields
-        }
-      }
-    }
-    allWordpressPage {
-      edges {
-        node {
-          id
-          title
-          excerpt
-          slug
-        }
-      }
-    }
-  }
-`
+export default IndexPage
